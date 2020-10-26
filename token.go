9b733b937e43c852b701f72ee132ce2d2e10e97d
Harish S.G <harishsg99@gmail.com>
2020-10-25 19:48:08 -0700

    Lexer done !!

---
+++ token.go
@@ -0,0 +1,53 @@
+
+package token
+
+type TokenType string
+
+type Token struct{
+  Type TokenType
+  Literal string
+ 
+ }
+
+const{
+    ILLEGAL = "ILLEGAL"
+    EOF = "EOF"
+    IDENT = "IDENT"
+    ASSIGN = "="
+    PLUS = "+"
+    MINUS = "-"
+    MULTIPLY = "*"
+    POWER = "^"
+    DIVIDE ="/"
+    COMMA = ","
+    SEMICOLON = ";"
+    LPAREN = "("
+    RPAREN = ")"
+    LBRACE = "{"
+    RBRACE = "}"
+    FUNCTION = "FUNCTION"
+	LET = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
+	EQ = "=="
+	NOT_EQ = "!="
+    
+}
+var keywords = map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
+	}
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+	return tok
+	}
+	return IDENT
+	}
