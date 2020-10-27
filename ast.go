69646dd67bb8f22bf4d45e71490a96e52cdddc1f
Harish S.G <harishsg99@gmail.com>
2020-10-26 20:01:51 -0700

    Parser done fully !!!

--- ast.go
+++ ast.go
@@ -46,7 +46,12 @@ type IfExpression struct {
 type BlockStatement struct {
      Token token.Token // the { token Statements []Statement
       }
-    
+type CallExpression struct {
+        Token token.Token // The '(' token
+        Function Expression
+        Arguments []Expression 
+        // Identifier or FunctionLiteral Arguments []Expression
+        }
 
 
 func (p *Program) TokenLiteral() string {
@@ -82,6 +87,11 @@ type Identifier struct {
 	Token token.Token // the token.IDENT token
 	Value string
 }
+type FunctionLiteral struct {
+    Token token.Token 
+    Parameters []*Identifier// The 'fn' token Parameters []*Identifier
+    Body *BlockStatement
+    }
 
 func (i *Identifier) expressionNode()            {}
 func (i *Identifier) TokenLiteral() string       { return i.Token.Literal }
@@ -158,4 +168,32 @@ for _, s := range bs.Statements {
     out.WriteString(s.String())
 }
 return out.String()
- }ß
+ }
+func (fl *FunctionLiteral) expressionNode() {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal } f
+func (fl *FunctionLiteral) String() string {
+var out bytes.Buffer
+params := []string{}
+for _, p := range fl.Parameters {
+params = append(params, p.String()) }
+out.WriteString(fl.TokenLiteral()) 
+out.WriteString("(") 
+out.WriteString(strings.Join(params, ", ")) 
+out.WriteString(") ") out.WriteString(fl.Body.String())
+return out.String()
+}
+func (ce *CallExpression) expressionNode() {}
+func (ce *CallExpression) TokenLiteral() string { 
+    return ce.Token.Literal } 
+func (ce *CallExpression) String() string {
+var out bytes.Buffer
+args := []string{}
+for _, a := range ce.Arguments {
+args = append(args, a.String()) 
+}
+out.WriteString(ce.Function.String())
+ out.WriteString("(")
+ out.WriteString(strings.Join(args, ", ")) 
+ out.WriteString(")")
+return out.String() 
+}
--- repl.go
+++ repl.go
@@ -4,25 +4,37 @@ import {
     "bufio"
     "fmt"
     "io"
-    "monkey/lexer"
-    "monkey/token"
+    "scooplang/lexer"
+    "scooplang/token"
 }
 
 const PROMPT = ">>"
 
-func start(in io.Reader  , out io.Writter){
-  scanner := bufio.NewScanner(in)
-   for {
-    fmt.Printf(PROMPT)
-    scanned := scanner.Scan()
-    if !scanned {
-    return
-} 
-line := scanner.Text()
-l := lexer.New(line)
-for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-fmt.Printf("%+v\n", tok)
-
-} 
-}
-}
+func Start(in io.Reader, out io.Writer) { 
+    scanner := bufio.NewScanner(in)
+    for {
+        fmt.Printf(PROMPT) 
+        scanned := scanner.Scan() 
+        if !scanned {
+            return
+    }
+    line := scanner.Text()
+    l := lexer.New(line)
+    p := parser.New(l)
+    program := p.ParseProgram() 
+    if len(p.Errors()) != 0 {
+        printParserErrors(out, p.Errors())
+        continue
+    }
+    io.WriteString(out, program.String())
+    io.WriteString(out, "\n") 
+    }
+    }
+    func printParserErrors(out io.Writer, errors []string) {
+        io.WriteString(out, "Woops! We ran into some Scoop business here!\n") 
+        io.WriteString(out, " parser errors:\n") 
+        for _, msg := range errors {
+            io.WriteString(out, "\t"+msg+"\n") 
+        }
+    }
+    
